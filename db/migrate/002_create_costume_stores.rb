9255474a225ca8d6a3bd057f31104675b853deb2
Sherry El Garhy <sherryptk@gmail.com>
2017-12-31 21:42:23 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-
+      t.boolean :still_in_business
       t.timestamps
     end
   end
