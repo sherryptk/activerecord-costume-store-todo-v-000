cd2d522b1aabfc08c68241030bcfda0199075c54
Sherry El Garhy <sherryptk@gmail.com>
2017-12-31 21:43:34 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
 
   def change
-    create_table :costumes do |t|
+    create_table :costumestores do |t|
       t.string :name
       t.string :location
       t.integer :costume_inventory
