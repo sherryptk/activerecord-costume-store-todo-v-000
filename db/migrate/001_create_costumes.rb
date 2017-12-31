7837c52bd0598ff9f0e10d3781d3afa52e17f0bf
Sherry El Garhy <sherryptk@gmail.com>
2017-12-31 21:35:56 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,6 +9,7 @@ class CreateCostumes < ActiveRecord::Migration
       t.integer :size
       t.timestamps
   end
+end
 # By convention, the class name should match the part of the
 # file name after the number, so in this case:
 # 002_create_costumes.rb becomes class CreateCostumes
