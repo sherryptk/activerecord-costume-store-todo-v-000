7887024d8f32af5bb8690f66bca9de1276e2c7e7
Sherry El Garhy <sherryptk@gmail.com>
2017-12-31 21:41:43 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+
+class CreateCostumes < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees_count
+
+      t.timestamps
+    end
+  end
+end
